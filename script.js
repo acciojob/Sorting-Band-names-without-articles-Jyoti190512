9006d955f1e08dfb2c478b73132509bdb2f4d11d
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-15 22:41:32 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+// Array of band names
+let bandNames = ['The Rolling Stones', 'Red Hot Chili Peppers', 'Led Zeppelin', 'Aerosmith', 'The Beatles'];
+
+// Function to remove articles from band names
+const removeArticles = (name) => {
+  return name.replace(/^(a|an|the)\s+/i, '');
+};
+
+// Sorting the band names in lexicographic order excluding articles
+bandNames = bandNames.map(removeArticles).sort();
+
+// Accessing the <ul> element with id 'band'
+const ulElement = document.getElementById('band');
+
+// Looping through the sorted band names and creating <li> elements
+bandNames.forEach((name) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
+
 
